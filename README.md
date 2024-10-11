a06177ab65976a626cac908d650433babb4f68c3
Odysseus <a.andrusyk@steel.mil.ua>
2024-10-11 12:48:09 +0300

    Improve README.md

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-NOT FINISHED YET
+# Simple server and client communicating via sockets in Python
+**NOT FINISHED YET**
 
-It lacks the error handling.
+This is an example of a server and client communicating via sockets in Python. Server generates some messages and 
+pushes it immediately to all connected clients. Both server and client require standard libraries only. Both 
+server and client were tested on `Python 3.12`.
+
+Run server:
+
+    python asyncio_socket_server_v1.py
+
+Run client:
+
+    python socket_client.py
+
+
+`asyncio_socket_server_v0` doesn't handle properly server interruption
+
+`asyncio_socket_server_v1` is a completely working solution, but may be improved. I'd like to:
+- avoid the coroutines switch by `await asyncio.sleep(0)`
+- avoid global variables usage as much as possible
+- avoid explicitly canceling the task with `t.cancel()`; add the exception-raising task as it is recommended in 
+the [documentation](https://docs.python.org/3/library/asyncio-task.html#terminating-a-task-group)
+- eliminate the ~2-second delay in notifying client disconnections on the server side; i.e. the delay in printing 
+`f'Socket {socname} closed'`
+
+`asyncio_socket_server_[under_construction].py` is the under construction version of server.
---
+++ asyncio_socket_server_[under_construction].py
@@ -0,0 +1,127 @@
+import asyncio
+import datetime as dt
+import struct
+
+from random import randrange
+from zoneinfo import ZoneInfo
+
+writers = []
+all_writers = {}
+weather = b''
+cancelled = False
+
+class TerminateTaskGroup(Exception):
+    """Exception raised to terminate a task group."""
+
+async def force_terminate_task_group():
+    """Used to force termination of a task group."""
+    while True:
+        print('force_terminate_task_group')
+        if cancelled:
+            raise TerminateTaskGroup()
+        await asyncio.sleep(0.5)
+
+async def prepare_weather_data():
+    global weather, writers, cancelled
+    try:
+        while True:
+            await asyncio.sleep(randrange(0, 7))
+            now = dt.datetime.now(ZoneInfo("Europe/Kyiv")).replace(microsecond=0).isoformat()
+            message = f"{now} The temperature is {str(randrange(20, 30))} degrees Celsius"
+
+            message_encoded = message.encode()
+            message_length = struct.pack('>I', len(message_encoded))
+
+            weather = message_length + message_encoded
+
+            writers = []
+            print(message)
+    except asyncio.CancelledError as e:
+        print('CANCELLED 1')
+        cancelled = True
+        # todo: important
+        # raise e
+        # raise KeyboardInterrupt
+
+
+async def read_message(reader: asyncio.StreamReader):
+    # Read the 4-byte length header
+    length_data = await reader.readexactly(4)
+    message_length = struct.unpack('>I', length_data)[0]
+    message_encoded = await reader.readexactly(message_length)
+    return message_encoded.decode()
+
+
+async def handle_client(
+    writer: asyncio.StreamWriter,
+):
+    global cancelled
+    socname = writer.get_extra_info('peername')
+    try:
+        while True:
+            # todo: refactor avoid sleep
+            await asyncio.sleep(0.5)
+            print('handle_client')
+            if socname not in writers:
+                writers.append(socname)
+                writer.write(weather)
+                await writer.drain()
+    except* ConnectionResetError:
+        del all_writers[socname]
+        writer.close()
+        print(f'Socket {socname} closed')
+    except* asyncio.CancelledError:
+        # todo: important
+        del all_writers[socname]
+        writer.close()
+        print('CANCELLED 2')
+        cancelled = True
+    except* Exception as e:
+        print('ERROR!')
+        raise e
+
+
+async def client_handler(reader, writer):
+    socname = writer.get_extra_info('peername')
+    all_writers[socname] = writer
+    message = await read_message(reader)
+    print(f"Socket {socname} connected. Received message: \"{message}\"")
+
+    await handle_client(writer)
+
+    print('DONE client_handler')
+
+
+async def run_server():
+    server = await asyncio.start_server(client_handler, "localhost", 8000)
+    print(f'Serving on {server.sockets[0].getsockname()}')
+    async with server:
+        try:
+            await server.serve_forever()
+        except asyncio.CancelledError as e:
+            # raise e # todo (AA): not sure if we need it
+            print('CANCELLED 3')
+
+        finally:
+            print('Server stopped.')
+            for writer_name in all_writers:
+                all_writers[writer_name].close()
+            print('sockets are closed')
+    print('DONE run_server')
+
+async def main():
+    try:
+        async with asyncio.TaskGroup() as group:
+            group.create_task(run_server(), name='server')
+            group.create_task(prepare_weather_data(), name='prepare_weather_data')
+            group.create_task(force_terminate_task_group())
+    except* asyncio.CancelledError:
+        print('DONE with asyncio.CancelledError')
+    except* KeyboardInterrupt:
+        print('DONE with KeyboardInterrupt')
+    except* Exception:
+        print('DONE with unknown exception')
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
